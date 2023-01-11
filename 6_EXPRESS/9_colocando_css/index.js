e4f2d0e5506880ec60c759f8fc0816eb761baf6b
Bruno de Souza Disliler <bruno_disliler@outlook.com>
2023-01-11 15:39:45 -0300

    Módulo 6 Express - Adicionando CSS

---
+++ 6_EXPRESS/9_colocando_css/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+const users = require('./users')
+const basePath = path.join(__dirname, 'templates')
+
+// Ler o Body
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+
+app.use(express.json())
+
+// Arquivos Estáticos
+app.use(express.static('public'))
+
+app.use('/users', users)
+
+app.get('/', (_req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`);
+})
